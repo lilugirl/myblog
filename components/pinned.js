f724b40651c3065e59f23e9654cb5010462a9a7f
liu yi <lilugirl2005@126.com>
2022-05-29 23:19:00 +0800

    变更

--- components/pinned.js
+++ components/pinned.js
@@ -1,7 +1,7 @@
 export default function Pinned(){
     return ( <div className="blog-header blog-is-sticky">
     <div className="blog-article header-article">
-      <div className="blog-big__title">Self</div>
+      <div className="blog-big__title">觉醒</div>
       <div className="blog-menu rounded small-title">Pinned Issue</div>
     </div>
     <div className="blog-article page-number">NO. 01</div>
